89bb1670b2f800cfff7780a3a20dab4d390db4f1
Erickkbonfim <141456725+Erickkbonfim27@users.noreply.github.com>
2025-02-28 22:28:51 -0300

    Update serializer.py

--- escola/serializer.py
+++ escola/serializer.py
@@ -10,7 +10,7 @@ class EstudanteSerializer(serializers.ModelSerializer):
         model = Estudante
         fields = "__all__"
 
-#validator geral
+    #validator geral com os dados d validator.py
     def validate(self, dados):
         if cpf_invalido(dados['cpf']):
             raise serializers.ValidationError({'cpf':"O CPF deve ter 11 digitos!"})
@@ -20,23 +20,6 @@ class EstudanteSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError({'numero_celular':"O numero de celular precisa ter 13 digitos"})
         return dados
 
-"""
-#validators individuais
-    def validate_cpf(self, cpf):
-        if len(cpf) != 11:
-            raise serializers.ValidationError("O CPF deve ter 11 digitos") #aqui é a mensagem de erro do  validator personalizado, da pra mandar um objeto tmb
-        return cpf
-    # o nome do metodo deve seguir esse padrao validate_nomedocampo
-    def validate_nome(self, nome):
-        if not nome.isalpha():
-            raise serializers.ValidationError("O nome não deve ter caracteres especiais!")
-        return nome
-
-    def validate_numero_celular(self, numero_celular):
-        if len(numero_celular) != 13:
-            raise serializers.ValidationError("O celular precisa ter 13 digitos")
-        return numero_celular
-"""
 
 class CursoSerializer(serializers.ModelSerializer):
     class Meta:
