c6d69fcd5cc4de6ccdcb2eee00074cf8076d7f24
Erickkbonfim <141456725+Erickkbonfim27@users.noreply.github.com>
2025-03-01 15:45:24 -0300

    Update settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -139,5 +139,6 @@ REST_FRAMEWORK = {
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.QueryParameterVersioning'
 }
